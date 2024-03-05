404839081b40c4b8c6c23b412fd727e5a6c20109
Serena758 <70768635+Serena748@users.noreply.github.com>
2024-03-04 18:33:03 -0800

    add review page basics

---
+++ src/pages/Review.js
@@ -0,0 +1,19 @@
+import React from "react"; 
+
+export default function Review(){ 
+
+    return(
+    <div> 
+       <p> Lessons </p>
+       <p> Flashcards </p> 
+       <p> Module Name </p> 
+
+    </div>
+
+
+    )
+    
+    
+    
+    
+    }
