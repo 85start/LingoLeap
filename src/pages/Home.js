2028c86283f4903fd623374f99424d9bdf6ec3df
Emily Yu <emilyyu331@gmail.com>
2024-04-04 01:23:33 -0700

    Matched homepage to Figma

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 import './Home.css'
 
-function Home() { 
+function Home() {
+   const navigate = useNavigate(); // Initialize useNavigate
+
 return(
    <div>
       <div className="home-links">
-            <span>
-              <Link to="/lessons"> Lessons </Link>
-              <Link to="/review"> Review </Link>
-              <Link to="/interviewprep"> Interview Prep </Link>
-              <Link to="/community"> Community </Link>
-            </span>
+         <div class="rectangle"></div>
+         <span>
+            <Link to="/lessons"> Lessons </Link>
+            <Link to="/review"> Review </Link>
+            <Link to="/interviewprep"> Interview Prep </Link>
+            <Link to="/community"> Community </Link>
+         </span>
+         <div class="startButton" onClick={() => { navigate("/lessons"); }}> Get Started </div>
+         <div class="circle"></div>
       </div> 
    </div>
    );
